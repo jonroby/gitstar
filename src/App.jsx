a5f2317d264ff53815881c915bf8cb4d1bc9d297
jonroby <joncaroby@gmail.com>
2019-02-18 11:40:34 -0800

    add apollo and github graphql api as client

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
+
+const TEST_QUERY = gql`
+  query {
+    repository(owner: "jonroby", name: "gitstar") {
+      name
+    }
+  }
+`;
 
 class App extends Component {
   render() {
-    return <div className="App">Gitstar</div>;
+    return (
+      <div className="App">
+        Gitstar
+        <Query query={TEST_QUERY}>
+          {({ loading, data }) => {
+            return !loading && <div>Hello Apollo</div>;
+          }}
+        </Query>
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import Routes from "./Routes";
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
 import * as serviceWorker from "./serviceWorker";
+import Routes from "./Routes";
 
-ReactDOM.render(<Routes />, document.getElementById("root"));
+import "./index.css";
+
+const client = new ApolloClient({
+  uri: "https://api.github.com/graphql"
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Routes />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
