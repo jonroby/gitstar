9c6af448759bddb881502e6633b3c8d056b9a7a8
jonroby <joncaroby@gmail.com>
2019-02-18 21:41:23 -0800

    reorganized and renamed list items to repo items

--- src/Repos.jsx
+++ src/Repos.jsx
@@ -1,116 +1,44 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { ApolloProvider, Query } from "react-apollo";
-import ApolloClient, { gql } from "apollo-boost";
+import { Query } from "react-apollo";
 import queryString from "query-string";
-import ListItem from "./ListItem";
-
-const accessToken = localStorage.getItem("access_token");
-
-const client = new ApolloClient({
-  uri: `https://api.github.com/graphql?access_token=${accessToken}`,
-});
-
-const STARRED_REPOS = cursor => {
-  const cursorPresent = cursor ? `, after: "${cursor}"` : "";
-  return gql`
-    query {
-      viewer {
-        starredRepositories(ownedByViewer: false, first: 2, ${cursorPresent}) {
-          pageInfo {
-	    startCursor
-	    endCursor
-	    hasNextPage
-            hasPreviousPage
-          }
-	  totalCount
-	  nodes {
-	    id,
-	    name       
-	    description,
-	    url
-	    updatedAt
-	    licenseInfo {
-	      name
-	    }
-	    owner {
-	      id
-	      login
-	    }
-	    primaryLanguage {
-	      name
-	    }
-	    stargazers {
-	      totalCount
-	    }
-            viewerHasStarred
-	  }
-        }
-      }
-    }
-  `;
-};
+import RepoItems from "./RepoItems";
+import STARRED_REPOS from "./queries/starredRepos";
 
 class Repos extends Component {
-  componentDidMount() {
-    if (!accessToken) {
-      this.props.history.push("/");
-    }
-  }
-
-  renderRepos(repos) {
-    return repos.map(metadata => {
-      return <ListItem key={metadata.id} metadata={metadata} />;
-    });
-  }
-
-  renderPagination(pageInfo) {
-    return (
-      <div>
-        {pageInfo.hasPreviousPage ? (
-          <Link to={`/repos?page=${pageInfo.startCursor}`}>Previous</Link>
-        ) : (
-          <div>No Previous</div>
-        )}
-        {pageInfo.hasNextPage ? (
-          <Link to={`/repos?page=${pageInfo.endCursor}`}>Next</Link>
-        ) : (
-          <div>No Next</div>
-        )}
-      </div>
-    );
-  }
+  // componentDidMount() {
+  //   if (!accessToken) {
+  //     this.props.history.push("/");
+  //   }
+  // }
 
   render() {
     const values = queryString.parse(this.props.location.search);
     const page = values.page || false;
 
     return (
-      <ApolloProvider client={client}>
-        <div>
-          Repos
-          <Query query={STARRED_REPOS(page)}>
-            {({ loading, data }) => {
-              const starredRepositories =
-                data &&
-                data.viewer &&
-                data.viewer.starredRepositories &&
-                data.viewer.starredRepositories;
+      <div>
+        Repos
+        <Query query={STARRED_REPOS(page)}>
+          {({ loading, data }) => {
+            const starredRepositories =
+              data &&
+              data.viewer &&
+              data.viewer.starredRepositories &&
+              data.viewer.starredRepositories;
 
-              if (!loading) {
-                return (
-                  <div>
-                    {this.renderRepos(starredRepositories.nodes)}
-                    {this.renderPagination(starredRepositories.pageInfo)}
-                  </div>
-                );
-              }
+            if (!loading) {
+              return (
+                <RepoItems
+                  repos={starredRepositories.nodes}
+                  pageInfo={starredRepositories.pageInfo}
+                />
+              );
+            }
 
-              return <div>Loading</div>;
-            }}
-          </Query>
-        </div>
-      </ApolloProvider>
+            return <div>Loading</div>;
+          }}
+        </Query>
+      </div>
     );
   }
 }
