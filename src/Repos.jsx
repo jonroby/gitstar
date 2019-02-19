ab7e4bc35a4b7c6cd3d3e2e3a1d7364adfa6d97b
jonroby <joncaroby@gmail.com>
2019-02-18 19:29:14 -0800

    setup pagination - broken

--- src/Repos.jsx
+++ src/Repos.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { ApolloProvider, Query } from "react-apollo";
 import ApolloClient, { gql } from "apollo-boost";
+import queryString from "query-string";
 import ListItem from "./ListItem";
 
 const accessToken = localStorage.getItem("access_token");
 
 const client = new ApolloClient({
-  uri: `https://api.github.com/graphql?access_token=${accessToken}`,
+  uri: `https://api.github.com/graphql?access_token=${accessToken}`
 });
 
 const STARRED_REPOS = cursor => {
@@ -14,11 +16,12 @@ const STARRED_REPOS = cursor => {
   return gql`
     query {
       viewer {
-        starredRepositories(ownedByViewer: false, first: 10 ${cursorPresent}) {
+        starredRepositories(ownedByViewer: false, first: 2, ${cursorPresent}) {
           pageInfo {
+	    startCursor
 	    endCursor
 	    hasNextPage
-	    startCursor
+            hasPreviousPage
           }
 	  totalCount
 	  nodes {
@@ -55,19 +58,55 @@ class Repos extends Component {
     }
   }
 
+  renderRepos(repos) {
+    return repos.map(metadata => {
+      return <ListItem key={metadata.id} metadata={metadata} />;
+    });
+  }
+
+  renderPagination(pageInfo) {
+    return (
+      <div>
+        {pageInfo.hasPreviousPage ? (
+          <Link to={`/repos?page=${pageInfo.startCursor}`}>Previous</Link>
+        ) : (
+          <div>No Previous</div>
+        )}
+        {pageInfo.hasNextPage ? (
+          <Link to={`/repos?page=${pageInfo.endCursor}`}>Next</Link>
+        ) : (
+          <div>No Next</div>
+        )}
+      </div>
+    );
+  }
+
   render() {
+    const values = queryString.parse(this.props.location.search);
+    const page = values.page || false;
+
     return (
       <ApolloProvider client={client}>
         <div>
           Repos
-          <Query query={STARRED_REPOS()}>
+          <Query query={STARRED_REPOS(page)}>
             {({ loading, data }) => {
-              return (
-                !loading &&
-                data.viewer.starredRepositories.nodes.map(metadata => {
-                  return <ListItem key={metadata.id} metadata={metadata} />;
-                })
-              );
+              const starredRepositories =
+                data &&
+                data.viewer &&
+                data.viewer.starredRepositories &&
+                data.viewer.starredRepositories;
+
+              if (!loading) {
+                return (
+                  <div>
+                    {this.renderRepos(starredRepositories.nodes)}
+                    {this.renderPagination(starredRepositories.pageInfo)}
+                  </div>
+                );
+              }
+
+              return <div>Loading</div>;
             }}
           </Query>
         </div>
