37f6a89f6e1a9bbcddbc155876a54568104d6781
jonroby <joncaroby@gmail.com>
2019-02-18 21:41:44 -0800

    move queries to own folder

---
+++ src/queries/starredRepos.js
@@ -0,0 +1,43 @@
+import { gql } from "apollo-boost";
+
+const STARRED_REPOS = cursor => {
+  const cursorPresent = cursor ? `, after: "${cursor}"` : "";
+  return gql`
+    query {
+      viewer {
+        starredRepositories(ownedByViewer: false, first: 2, ${cursorPresent}) {
+          pageInfo {
+	    startCursor
+	    endCursor
+	    hasNextPage
+            hasPreviousPage
+          }
+	  totalCount
+	  nodes {
+id,
+name
+description,
+url
+updatedAt
+licenseInfo {
+  name
+}
+owner {
+  id
+  login
+}
+primaryLanguage {
+  name
+}
+stargazers {
+  totalCount
+}
+viewerHasStarred
+
+	  }
+        }
+      }
+    }`;
+};
+
+export default STARRED_REPOS;
