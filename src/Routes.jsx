964467e273f443213524e60f789252a4d13c6f94
jonroby <joncaroby@gmail.com>
2019-02-18 23:36:21 -0800

    use app container for centered app content

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,22 +1,28 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import styled from "styled-components/macro";
 import App from "./App";
 import OAuth from "./OAuth";
 import Repos from "./Repos";
 import Search from "./Search";
 
+const AppContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
 class Routes extends Component {
   render() {
     return (
       <BrowserRouter>
-        <React.Fragment>
+        <AppContainer>
           <Switch>
             <Route path="/" component={App} exact />
             <Route path="/oauth" component={OAuth} />
             <Route path="/starred" component={Repos} />
             <Route path="/search" component={Search} />
           </Switch>
-        </React.Fragment>
+        </AppContainer>
       </BrowserRouter>
     );
   }
